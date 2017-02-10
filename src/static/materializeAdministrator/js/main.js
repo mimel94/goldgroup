d96d3dc47eafc1811db1910d1fd2c30d4a17fd5f
Miller <millergarciaq@gmail.com>
2017-02-10 12:54:41 -0500

    added function of materialize for form create salesman

--- src/static/materializeAdministrator/js/main.js
+++ src/static/materializeAdministrator/js/main.js
@@ -11,6 +11,11 @@ $(document).ready(function(){
         }
 
     });
+    $('select').material_select();
+    $('.datepicker').pickadate({
+     selectMonths: true, // Creates a dropdown to control month
+     selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
     $('.tooltipped').tooltip({delay: 50});
     $('.ShowHideMenu').on('click', function(){
         var MobileMenu=$('.NavLateral');
