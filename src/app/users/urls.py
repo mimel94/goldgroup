010fdef4d4a265c09f88328d551a131c891e8562
Miller <millergarciaq@gmail.com>
2017-02-17 14:50:58 -0500

    added urls for more information salesman and update data of salesman

--- src/app/users/urls.py
+++ src/app/users/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.contrib.auth.views import logout
-from .views import Administrator, CreateSalesman, ActiveSalesman, ActiveSalesmanUpdateState
+from .views import Administrator, CreateSalesman, ActiveSalesman, ActiveSalesmanUpdateState,\
+                   MoreInfoSalesman, UpdateSalesman
+
 
 urlpatterns = [
     url(r'^administrador/inicio/$',Administrator.as_view(), name='index_administrator'),
@@ -11,6 +13,10 @@ urlpatterns = [
     url(r'^administrador/vendedores_inactivos/$',ActiveSalesman.as_view(active="no"), name='inactives_saleman'),
     url(r'^administrador/vendedores_actualizar/estado/(?P<pk>\d+)/$',ActiveSalesmanUpdateState.as_view(),
         name='update_state_salesman'),
+    url(r'^administrador/vendedores/mas_informacion/(?P<pk>\d+)/$',MoreInfoSalesman.as_view(),
+        name='more_info_salesman'),
+    url(r'^administrador/vendedores/actualizar/(?P<pk>\d+)/$',UpdateSalesman.as_view(),
+        name='update_salesman'),
 
 
 ]
